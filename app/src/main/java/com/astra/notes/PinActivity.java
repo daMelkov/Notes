a66ee577eb3386005fb6f97df5bafebca02687cf
Melkov Denis <d.a.melkov@gmail.com>
2020-09-27 22:30:31 +0300

    pin-buttons onClick

--- app/src/main/java/com/astra/notes/PinActivity.java
+++ app/src/main/java/com/astra/notes/PinActivity.java
@@ -1,11 +1,20 @@
 package com.astra.notes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Stack;
+
 public class PinActivity extends AppCompatActivity {
+    private static final int PINS_COUNT = 4;
+    private static Stack<Integer> PINS = new Stack<>();
+
+    private int currentPin = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +24,73 @@ public class PinActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        ;
+        Button btnOne = findViewById(R.id.button_one);
+        btnOne.setOnClickListener(onClick);
+
+        Button btnTwo = findViewById(R.id.button_two);
+        btnTwo.setOnClickListener(onClick);
+
+        Button btnThree = findViewById(R.id.button_three);
+        btnThree.setOnClickListener(onClick);
+
+        Button btnFour = findViewById(R.id.button_four);
+        btnFour.setOnClickListener(onClick);
+
+        Button btnFive = findViewById(R.id.button_five);
+        btnFive.setOnClickListener(onClick);
+
+        Button btnSix = findViewById(R.id.button_six);
+        btnSix.setOnClickListener(onClick);
+
+        Button btnSeven = findViewById(R.id.button_seven);
+        btnSeven.setOnClickListener(onClick);
+
+        Button btnEight = findViewById(R.id.button_eight);
+        btnEight.setOnClickListener(onClick);
+
+        Button btnNine = findViewById(R.id.button_nine);
+        btnNine.setOnClickListener(onClick);
+
+        Button btnZero = findViewById(R.id.button_zero);
+        btnZero.setOnClickListener(onClick);
+
+        Button btnDelete = findViewById(R.id.button_delete);
+        btnDelete.setOnClickListener(onDelete);
+
+        Button btnCancel = findViewById(R.id.button_cancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PINS.size();
+            }
+        });
     }
+
+    /* 0...9 */
+    View.OnClickListener onClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int number = Integer.parseInt(String.valueOf(((Button) view).getText()));
+
+            if(currentPin == PINS_COUNT) {
+                return;
+            }
+
+            PINS.add(number);
+            currentPin++;
+        }
+    };
+
+    /* delete */
+    View.OnClickListener onDelete = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(currentPin == 0) {
+                return;
+            }
+
+            PINS.pop();
+            currentPin--;
+        }
+    };
 }
